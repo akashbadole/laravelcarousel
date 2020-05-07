5d9e6d5636b22d3fb5d6c968ad2f2ee41a138e31
unknown <akashbadole@rediffmail.com>
2020-05-07 10:37:09 +0530

    cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -233,6 +233,40 @@
 
     <!-- /END THE FEATURETTES -->
 
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-4">
+            <div class="card" width= "250px" height="auto">
+                <img src="https://picsum.photos/id/237/200/300?grayscale" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card" width= "250px" height="auto">
+                <img src="https://picsum.photos/seed/picsum/200/300?grayscale" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card" width= "250px" height="auto">
+                <img src="https://picsum.photos/200/300?grayscale" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+
+
+      </div>
+    </div>
+
   </div><!-- /.container -->
 
 
